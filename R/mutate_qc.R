ca3b066c28f1dad99915b294eb34280b7a017484
Adam <adam.maier@tntp.org>
2018-02-22 13:49:20 -0500

    Added scoped variants

--- R/mutate_qc.R
+++ R/mutate_qc.R
@@ -8,8 +8,8 @@
 #' \code{mutate_qc} and \code{transmute_qc} are used exactly the same as 
 #' \code{mutate} and \code{transmute} and require all of the same arguments
 #' and return identical objects. The only difference is that the \code{_qc}
-#' versions print a message indicating the number of NA values created in the
-#' new or edited variable(s) after calling \code{mutate}. 
+#' versions print a message indicating the number of NA or INFinite values 
+#' created in the new or edited variable(s) after calling \code{mutate}. 
 #' 
 #' @section Scoped mutation and transmutation:
 #'   Currently there are not _qc versions of \code{mutate_at}, 
@@ -29,27 +29,60 @@
 #' @seealso \code{\link[dplyr]{mutate}}
 #' 
 #' @examples 
-#' practice_data <- data.frame(A = c(1:4, NA), B = c(NA, 7:10), C = 21:25)
-#' 
-#' mutate_qc(practice_data, new_var = A + B)
-#' 
-#' transmute_qc(
-#'   practice_data,
-#'   new_var_1 = A + B,
-#'   new_var_2 = A - C
+#' practice_data <- 
+#'   data.frame(
+#'   A = c(1:4, NA), 
+#'   B = c(NA, 7:10), 
+#'   C = 21:25,
+#'   G = c("X", "X", "X", "Y", "Y"),
+#'   stringsAsFactors = F
 #' )
+#' 
+#' # Use the _qc versions just like normal dplyr mutate functions
+#' mutate_qc(practice_data, new_var_1 = A + B, new_var_2 = A - C)
+#' mutate_at_qc(practice_data, vars(A, C), funs(m = mean(., na.rm = T), s = sum))
+#' 
+#' # This will throw a warning because G is a character, but will still work.
+#' mutate_all_qc(practice_data, funs(mean))
+#' 
+#' # Functions worked on grouped data, too
+#' grouped_data <- dplyr::group_by(practice_data, G)
+#' mutate_qc(grouped_data, new_var_1 = A + mean(B), mean_b = mean(B))
+#' mutate_at_qc(grouped_data, vars(A, C), funs(m = mean(., na.rm = T), s = sum))
 
-# Helper Function
-na_counter <- function(.data, ...){
+
+
+# EXPORTED FUNCTIONS ----------------------------------------------------------- 
+
+na_counter <- function(.data, .vars, .funs, .predicate, ...){
 
   # Counting number of NAs in each mutate call by making each new variable then 
   # counting NAs in each and storing as list element with appropriate var name.
   # Separate processes if data is grouped
+  
+  # Creating new variables representing the function which calls na_counter
+  fn_name <- gsub("\\(.*", "", deparse(sys.call(-1)[[1]]))
+
+  if (fn_name %in% c("mutate_qc", "transmute_qc")) {
+    new_vars <- suppressMessages(dplyr::transmute(.data, ...))
+  }
+  
+  if (fn_name %in% c("mutate_all_qc", "transmute_all_qc")) {
+    new_vars <- suppressMessages(dplyr::transmute_all(.data, .funs, ...))
+  }
+  
+  if (fn_name %in% c("mutate_at_qc", "transmute_at_qc")) {
+    new_vars <- suppressMessages(dplyr::transmute_at(.data, .vars, .funs, ..., .cols = NULL))
+  }
+  
+  if (fn_name %in% c("mutate_if_qc", "transmute_if_qc")) {
+    new_vars <- suppressMessages(dplyr::transmute_if(.data, .predicate, .funs, ...))
+  }
     
   # Ungrouped
   if (is.null(attr(.data, "vars"))) {
         
-    new_vars <- dplyr::transmute(.data, ...)
+    #new_vars <- dplyr::transmute(.data, ...)
     num_na <- 
       dplyr::summarize_all(
         new_vars, 
@@ -67,25 +100,29 @@ na_counter <- function(.data, ...){
         
     group_var <- attr(.data, "vars")
         
-      new_vars <- suppressMessages(dplyr::transmute(.data, ...))
-      new_vars <- dplyr::ungroup(new_vars)
-        
-      keep_vars <- names(new_vars)[!names(new_vars) %in% group_var]
-      new_vars <- dplyr::select_at(new_vars, keep_vars)
-        
-      num_na <- 
-        dplyr::summarize_all(
-          new_vars, 
-          dplyr::funs(sum(is.na(.) | is.infinite(.)))
-        )
-        
-      mapply(
-        FUN = function(x, y) message(x, " NAs or INFs produced in ", y), 
-        x = num_na, y = names(num_na)
+    #new_vars <- suppressMessages(dplyr::transmute(.data, ...))
+    new_vars <- dplyr::ungroup(new_vars)
+      
+    keep_vars <- names(new_vars)[!names(new_vars) %in% group_var]
+    new_vars <- dplyr::select_at(new_vars, keep_vars)
+      
+    num_na <- 
+      dplyr::summarize_all(
+        new_vars, 
+        dplyr::funs(sum(is.na(.) | is.infinite(.)))
       )
+      
+    mapply(
+      FUN = function(x, y) message(x, " NAs or INFs produced in ", y), 
+      x = num_na, y = names(num_na)
+    )
   }
     
 }
+
+
+
+# EXPORTED FUNCTIONS ----------------------------------------------------------- 
 
 #' @rdname mutate_qc
 #' @export
@@ -95,7 +132,7 @@ mutate_qc <- function(.data, ...){
   out <- dplyr::mutate(.data, ...)
    
   # Print NAs
-  na_counter(.data, ...)
+  na_counter(.data = .data, ...)
   
   # Returning data
   return(out)
@@ -110,10 +147,99 @@ transmute_qc <- function(.data, ...){
   out <- dplyr::transmute(.data, ...)
   
   # Print NAs
-  na_counter(.data, ...)
+  na_counter(.data = .data, ...)
   
   # Returning data
   return(out)
   
 }
+
+#' @rdname mutate_qc
+#' @export
+mutate_all_qc <- function(.tbl, .funs, ...){
     
+  # Performing mutate
+  out <- dplyr::mutate_all(.tbl, .funs, ...)
+  
+  # Print NAs
+  na_counter(.data = .tbl, .funs = .funs, ...)
+  
+  # Returning data
+  return(out)
+  
+}
+
+#' @rdname mutate_qc
+#' @export
+transmute_all_qc <- function(.tbl, .funs, ...){
+  
+  # Performing mutate
+  out <- dplyr::transmute_all(.tbl, .funs, ...)
+  
+  # Print NAs
+  na_counter(.data = .tbl, .funs = .funs, ...)
+  
+  # Returning data
+  return(out)
+  
+}
+
+#' @rdname mutate_qc
+#' @export
+mutate_at_qc <- function(.tbl, .vars, .funs, ..., .cols = NULL){
+  
+  # Performing mutate
+  out <- dplyr::mutate_at(.tbl, .vars, .funs, ..., .cols = NULL)
+  
+  # Print NAs
+  na_counter(.data = .tbl, .vars = .vars, .funs = .funs, ...)
+  
+  # Returning data
+  return(out)
+  
+}
+
+#' @rdname mutate_qc
+#' @export
+transmute_at_qc <- function(.tbl, .vars, .funs, ..., .cols = NULL){
+  
+  # Performing mutate
+  out <- dplyr::transmute_at(.tbl, .vars, .funs, ..., .cols = NULL)
+  
+  # Print NAs
+  na_counter(.data = .tbl, .vars = .vars, .funs = .funs, ...)
+  
+  # Returning data
+  return(out)
+  
+}
+
+#' @rdname mutate_qc
+#' @export
+mutate_if_qc <- function(.tbl, .predicate, .funs, ...){
+  
+  # Performing mutate
+  out <- dplyr::mutate_if(.tbl, .predicate, .funs, ...)
+  
+  # Print NAs
+  na_counter(.data = .tbl, .predicate = .predicate, .funs = .funs, ...)
+  
+  # Returning data
+  return(out)
+  
+}
+
+#' @rdname mutate_qc
+#' @export
+transmute_if_qc <- function(.tbl, .predicate, .funs, ...){
+  
+  # Performing mutate
+  out <- dplyr::transmute_if(.tbl, .predicate, .funs, ...)
+  
+  # Print NAs
+  na_counter(.data = .tbl, .predicate = .predicate, .funs = .funs, ...)
+  
+  # Returning data
+  return(out)
+  
+}
